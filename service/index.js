3451b49d8cb523a29ff230598e8a23bead368e27
nguyenduckhanh2003 <khanhndhe171361@fpt.edu.vn>
2025-04-14 22:57:28 +0700

    first commit

---
+++ service/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const initRoutes = require('./routes/index.route');
+const connectDB = require('./config/mongo');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = process.env.PORT || 9000;
+const http = require('http');
+const server = http.createServer(app);
+app.use(cors(
+  {
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Content-Disposition'],
+  }
+));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+connectDB();
+initRoutes(app);
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

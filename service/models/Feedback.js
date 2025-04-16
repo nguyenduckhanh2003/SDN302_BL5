d3ec52a614e88ae49186986f29763605868df20e
nguyenduckhanh2003 <khanhndhe171361@fpt.edu.vn>
2025-04-17 00:53:54 +0700

    config modal

--- service/models/Feedback.js
+++ service/models/Feedback.js
@@ -1,28 +1,17 @@
 const mongoose = require('mongoose');
 
 const feedbackSchema = new mongoose.Schema({
-    userId: {
+    storeId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'Store',
         required: true,
     },
-    averageRating: {
+    rating: {
         type: Number,
         required: true,
         min: 1,
         max: 5,
-    },
-    totalReviews: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
-    positiveRate: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 100,
-    },
+    }
 });
 
 const Feedback = mongoose.model('Feedback', feedbackSchema);
